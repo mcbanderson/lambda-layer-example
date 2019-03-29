1094d7147700edc80414243ed73a812a8c459796
Max Anderson <maxwell.c.anderson@gmail.com>
2019-03-28 17:12:13 -0700

    Update template to include layerversion and parameter. Create my_module

---
+++ src/my_module.py
@@ -0,0 +1,9 @@
+""" A sample module to be used as a lambda layer """
+import numpy as np
+
+
+def my_function():
+    """ Prints and returns numpy's constant for e """
+    print(np.e)
+
+    return np.e
